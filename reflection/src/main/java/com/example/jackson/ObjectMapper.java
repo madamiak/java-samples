e61bf62ba30a1d4e40a421a6527f43f409b03326
Mateusz <amalgammatt@gmail.com>
2018-04-13 17:38:10 +0200

    Resolved exercise

--- reflection/src/main/java/com/example/jackson/ObjectMapper.java
+++ reflection/src/main/java/com/example/jackson/ObjectMapper.java
@@ -2,29 +2,35 @@ package com.example.jackson;
 
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Iterator;
 
 public class ObjectMapper {
     public <T> T readValue(String json, Class<T> type) {
         try {
             // utwórz instancję typu T
-            // kawałek kodu: type.<metoda do tworzenia obiektu/instancji>()
+            T instance = type.newInstance();
 
             JSONObject jsonObject = new JSONObject(json);
             Iterator<String> keys = jsonObject.keys();
             while (keys.hasNext()) { //dla każdego klucza w JSONie
                 // pobierz wartość klucza
-                // kawałek kodu: jsonObject.get("<nazwa klucza w JSONie>");
+                String key = keys.next();
+
+                // pobierz typ pola
+                Field declaredField = type.getDeclaredField(key);
+                Object value = jsonObject.get(key);
 
                 // ustaw klucz we wcześniej utworzonym obiekcie
-                // kawałek kodu: type.getMethod("set<nazwa klucz w JSONie z pierwsza wielką literą")
+                Method method = type.getMethod("set" + key.substring(0, 1).toUpperCase() + key.substring(1), declaredField.getType());
 
                 // wywołaj metodę
-                // kawałek kodu method.invoke(<obiekt utworzony w linijce 10>, <wartość klucza>)
+                method.invoke(instance, value);
             }
 
             // zwróć obiekt z linijki 10
-            return null;
+            return instance;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
